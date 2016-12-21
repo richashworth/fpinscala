8b6b34c0b1871594140350409dfcf0802652f2d2
Richard Ashworth <richardashworth575@gmail.com>
2016-12-21 20:34:57 +0100

    removed annotation

--- exercises/src/main/scala/fpinscala/datastructures/Trees.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Trees.scala
@@ -8,7 +8,9 @@ import scala.annotation.tailrec
 object Trees {
 
   sealed trait Tree[+A]
+
   case class Leaf[A](value: A) extends Tree[A]
+
   case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
   def size[A](tree: Tree[A]): Int = tree match {

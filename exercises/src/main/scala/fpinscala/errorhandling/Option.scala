10c977c1a0adca711a11a146bd8d7914b7f7f3e9
Richard Ashworth <richardashworth575@gmail.com>
2016-12-28 17:46:44 +0000

    formatted src with scalafmt

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -18,7 +18,8 @@ sealed trait Option[+A] {
 
   def orElse[B >: A](ob: => Option[B]): Option[B] = map(Some(_)).getOrElse(ob)
 
-  def filter(f: A => Boolean): Option[A] = flatMap(a => if (f(a)) Some(a) else None)
+  def filter(f: A => Boolean): Option[A] =
+    flatMap(a => if (f(a)) Some(a) else None)
 }
 
 case class Some[+A](get: A) extends Option[A]
@@ -31,8 +32,7 @@ object Option {
     try {
       val x = 42 + 5
       x + y
-    }
-    catch {
+    } catch {
       case e: Exception => 43
     } // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
   }
@@ -41,8 +41,7 @@ object Option {
     try {
       val x = 42 + 5
       x + ((throw new Exception("fail!")): Int) // A thrown Exception can be given any type; here we're annotating it with the type `Int`
-    }
-    catch {
+    } catch {
       case e: Exception => 43
     }
   }
@@ -52,11 +51,14 @@ object Option {
     else Some(xs.sum / xs.length)
 
   //if the mean of a seq is m then the variance is the mean of (x-m)^2 for all x in seq
-  def variance(xs: Seq[Double]): Option[Double] = mean(xs).flatMap((m) => mean(xs.map((x) =>math.pow((x - m),2))))
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs).flatMap((m) => mean(xs.map((x) => math.pow((x - m), 2))))
 
-  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    sys.error("todo")
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    sys.error("todo")
 }
